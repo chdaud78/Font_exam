f22a14edb41c73b78fb32efaeac6816c1cef9750
chdaud78 <57937900+chdaud78@users.noreply.github.com>
2020-09-18 18:59:50 +0900

    add fonts example ad javascript

---
+++ function.js
@@ -0,0 +1,7 @@
+
+
+function update(){
+    var sampleText = document.getElementById("sample").value;
+    console.log(sampleText);
+    document.getElementById("sampleText").innerHTML = sampleText;
+}   
